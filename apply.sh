ae954e6023eb29e0984d66667fe911243964da7b
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-08-19 13:25:46 +1000

    DEBUG = False

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 # apply new config
-echo "copying $CP_THEME_HOME/config.py to $CP_HOME/catprez/config.py"
-$CP_DATA_DIR = $CP_THEME_HOME/data
+echo "update $CP_THEME_HOME/config.py with data dir ($CP_THEME_HOME/data)"
+CP_DATA_DIR=$CP_THEME_HOME/data
 sed -i "s#CP_DATA_DIR#$CP_DATA_DIR#" $CP_THEME_HOME/config.py
+echo "copying $CP_THEME_HOME/config.py to $CP_HOME/catprez/config.py"
 cp $CP_THEME_HOME/config.py $CP_HOME/catprez/config.py
 
 # copy all style content to CP
